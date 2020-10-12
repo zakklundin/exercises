8cc3114203b32b7351eabd479c0fef0829e89e6a
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-10-12 15:27:44 +0200

    solved 17

---
+++ A-Beginner/17/script.js
@@ -0,0 +1,10 @@
+function isTanja(a){
+    if (a === "Tanja") {
+        return true
+    } else {return false}
+}
+
+//sätter man in "Tanja" som argument nedan ska man få true
+let result = isTanja("Kim")
+
+console.log(result)
